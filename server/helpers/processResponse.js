673a1ab7459faccb69e066c148b6fd81bf42f111
Sam <sam@blackarcs.org>
2022-03-05 20:27:06 -0400

    validateData

--- server/helpers/processResponse.js
+++ server/helpers/processResponse.js
@@ -3,7 +3,18 @@ const processResponse = () => {
 }
 
 const validateData = (country) => {
-    
+    Object.keys(country).forEach((attr) => {
+        let val = country[attr];
+
+       if(attr === 'languages' && val.length === 0) {
+           val.push('No languages available');
+       } 
+
+       if(!val) val = `No ${attr} available`;
+    });
+
+
+    return country;
 }
 
 const sortCountries = () => {
