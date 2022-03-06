68f8b2b0e87b843fc461bd7425b85d5797067cf3
Sam <sam@blackarcs.org>
2022-03-05 23:16:11 -0400

    added comments

--- src/server/helpers/processResponse.js
+++ src/server/helpers/processResponse.js
@@ -1,3 +1,9 @@
+/**
+ * Verifies that all data has a value
+ * Fills in missing data
+ * @param {Object} country 
+ * @returns 
+ */
 const validateData = (country) => {
     Object.keys(country).forEach((attr) => {
        if(attr === 'languages' && country[attr].length === 0) {
@@ -12,12 +18,22 @@ const validateData = (country) => {
     return country;
 }
 
+/**
+ * Sorts all countries in descending order by code
+ * @param {Array<Object>} countries 
+ * @returns 
+ */
 const sortCountries = (countries) => {
     let res = countries;
     res.sort((a, b) => {return b.code - a.code});
     return res;
 }
 
+/**
+ * Calculates all the data to be used in the summary
+ * @param {Array<Object>} countries 
+ * @returns 
+ */
 const calculateSummaryData = (countries) => {
     let summary = {
         regions: {},
@@ -27,12 +43,15 @@ const calculateSummaryData = (countries) => {
 
     countries.forEach((country) => {
         let {region, subregion} = country;
+
+        //gets summary for regions
         if(summary.regions[region]) {
             summary.regions[region].push(country.name); 
         } else {
             summary.regions[region] = [country.name]; 
         }
 
+        //gets summary for subregions
         if(summary.subregions[subregion]) {
             summary.subregions[subregion].push(country.name); 
         } else {
