370ee3ffac11e2419bd84700148086e6174dde64
Sam <sam@blackarcs.org>
2022-03-06 00:45:27 -0400

    implement client api - iteration one

--- src/client/api/api.js
+++ src/client/api/api.js
@@ -1,13 +1,55 @@
 export default class API {
-    static async getCountriesByCode (query, setCountries) {
+    static port = 4000;
+    static baseURL = `http://localhost:${API.port}`
 
+    static async getCountriesByCode (query, setCountries) {
+        const url = `${API.baseURL}/code/${query}`;
+        const options = {
+            method: 'GET'
+        };
+
+        let results;
+        try {
+            let response = await fetch(url, options);
+            results = await JSON.parse(response);
+        } catch (e) {
+            console.error(e);
+        }
+
+        return results;
     }
 
     static async getCountriesByName (query, setCountries) {
-        
+        const url = `${API.baseURL}/name/${query}`;
+        const options = {
+            method: 'GET'
+        };
+
+        let results;
+        try {
+            let response = await fetch(url, options);
+            results = await JSON.parse(response);
+        } catch (e) {
+            console.error(e);
+        }
+
+        return results;
     }
 
     static async getCountriesByFullName (query, setCountries) {
-        
+        const url = `${API.baseURL}/fullName/${query}`;
+        const options = {
+            method: 'GET'
+        };
+
+        let results;
+        try {
+            let response = await fetch(url, options);
+            results = await JSON.parse(response);
+        } catch (e) {
+            console.error(e);
+        }
+
+        return results;
     }
 }
