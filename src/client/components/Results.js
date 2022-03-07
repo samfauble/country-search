0dea442032db777d8a67712015c9be35d4cd2e55
Sam <sam@blackarcs.org>
2022-03-06 21:23:00 -0400

    tables displaying

--- src/client/components/Results.js
+++ src/client/components/Results.js
@@ -1,15 +1,52 @@
 import React from 'react';
+import {v4 as uuid} from 'uuid';
 
 export default function Results (props) {
     let { info } = props;
     let { countries } = info;
-    const names = countries.map((c,i) => {
-        return (<li key={i}>{c.name.official}</li>)
+    const tableRows = [];
+    const headerId = uuid();
+    const headers = (
+        <tr key={headerId}>
+            <th>full_name</th>
+            <th>alpha_code_2</th>
+            <th>alpha_code_3</th>
+            <th>flag</th>
+            <th>region</th>
+            <th>subregion</th>
+            <th>population</th>
+            <th>languages</th>
+        </tr>
+    )
+
+    tableRows.push(headers);
+
+    countries.forEach((c, i) => {
+        const languages = Object.keys(c.languages).map((l) => {
+            return ` ${c.languages[l]} `;
+        });
+        let id = uuid();
+        const row = (
+            <tr key={id}>
+                <td>{c.name.official}</td>
+                <td>{c.cca2}</td>
+                <td>{c.cca3}</td>
+                <td><img src={c.flags.png} alt='flag' /></td>
+                <td>{c.region}</td>
+                <td>{c.subregion}</td>
+                <td>{c.population}</td>
+                <td>{languages}</td>
+            </tr>
+        );
+
+        tableRows.push(row);
     });
 
     return (
-        <ul>
-            {names}
-        </ul>
+        <table>
+            <tbody>
+                {tableRows}
+            </tbody>
+        </table>
     )
 }
--- src/client/components/Summary.js
+++ src/client/components/Summary.js
@@ -1,15 +1,68 @@
-import React from 'react';
+import React, {Fragment} from 'react';
+import {v4 as uuid} from 'uuid';
 
 export default function Summary (props) {
     const { info } = props;
     const { summary } = info;
+    const regionTableRows = [];
+    const headerKey = uuid();
+    const regionHeaders = (
+        <tr key={headerKey}>
+            <th>region</th>
+            <th>num_countries_in_region</th>
+        </tr>
+    )
+    regionTableRows.push(regionHeaders);
+
+    const subregionTableRows = [];
+    const headerId = uuid();
+    const subregionHeaders = (
+        <tr key={headerId}>
+            <th>subregion</th>
+            <th>num_countries_in_subregion</th>
+        </tr>
+    )
+    subregionTableRows.push(subregionHeaders);
+
+    Object.keys(summary.regions).forEach((r, i) => {
+        let arr = summary.regions[r];
+        let id = uuid();
+        const row = (
+            <tr key={id}>
+                <td>{r}</td>
+                <td>{arr.length}</td>
+            </tr>
+        );
+        regionTableRows.push(row);
+    });
+
+    Object.keys(summary.subregions).forEach((s, i) => {
+        let arr = summary.subregions[s];
+        let id = uuid();
+        const row = (
+            <tr key={id}>
+                <td>{s}</td>
+                <td>{arr.length}</td>
+            </tr>
+        );
+        subregionTableRows.push(row);
+    });
 
     return (
-        <ul>
-            {Object.keys(summary.regions).map(
-                (r,i) => {
-                    return (<li key={i}>{r}</li>)
-                })}
-        </ul>
+        <Fragment>
+            <h3>Total Number of countries: {summary.total}</h3>
+            <h4>Regions</h4>
+            <table>
+                <tbody>
+                    {regionTableRows}
+                </tbody>
+            </table>
+            <h4>Subregions</h4>
+            <table>
+                <tbody>
+                    {subregionTableRows}
+                </tbody>
+            </table>
+        </Fragment>
     )
 }
